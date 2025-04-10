dd558c7b07f8bffce535af858c616a27d063218d
Luka Castineira <castineiraluka@gmail.com>
2025-04-10 00:32:11 -0300

    Desafio 01 completados por Luka Castineira

---
+++ Practicas/Practica-01/Js/saludoPersonalizado.js
@@ -0,0 +1,7 @@
+function ingresarNombre (){
+    let userName = prompt(" Ingrese su nombre: "); // pide al usuario ingresar su nombre
+    alert(' Hola ' + userName + '!'); // lanza una alerta despues de ingresar el nombre
+
+}
+
+ingresarNombre(); // se ejecuta la funcion
