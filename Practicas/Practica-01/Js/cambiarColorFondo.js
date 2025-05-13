9e6546d49f96f21b7e3e77ffe9ace148075f9bd2
Luka Castineira <castineiraluka@gmail.com>
2025-05-13 00:02:33 -0300

    Agrego nuevas practicas resueltas

--- Practicas/Practica-01/Js/cambiarColorFondo.js
+++
@@ -1,9 +0,0 @@
-function colorAleatorio(){
-    let r = Math.floor(Math.random()*256); // Aleatorio de rojo
-    let g = Math.floor(Math.random()*256); // Aleatorio de verde
-    let b = Math.floor(Math.random()*256); // Aleatorio de azul
-    return `rgb(${r}, ${g}, ${b})`; // me da todo en un string "rgb(..,..,..)" 
-}  // importante usar `` y no '' !!! si no no te deja concatenar variables!!
-
-document.body.style.backgroundColor = colorAleatorio();
-
